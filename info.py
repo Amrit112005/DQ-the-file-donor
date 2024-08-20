baba25780815bd865578651e268f0d0f7479b2d8
Youesky <135997849+youesky@users.noreply.github.com>
2024-08-20 18:28:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,16 +45,10 @@ NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 SECONDDB_URI = environ.get('SECONDDB_URI', None)
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 # upstream
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/Amrit112005/DQ-the-file-donor",
-)
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)
+UPSTREAM_REPO = environ.get('UPSTREAM_REPO', 'https://github.com/Amrit112005/DQ-the-file-donor')
+UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', 'main')
+GIT_TOKEN = environ.get('GIT_TOKEN', None)
 # Others
 IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/+tO_VMvYw6lNlOWE1")
